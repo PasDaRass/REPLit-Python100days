cc014532dcfec9a6b214db40d946fcd8d3da7cb6
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-09-13 18:52:55 +0100

    Update main.py

--- Day5/main.py
+++ Day5/main.py
@@ -1,1 +1,17 @@
+# If/ Else Statements
 
+print("Which character are you? Harry Potter Edition")
+print("--")
+answer1 = input("Are you in Gryffindor? ")
+if answer1.lower() == "no":
+  print("You are Malfoy")
+else:
+  answer2 = input("Do you wear glasses? ")
+  if answer2.lower() == "yes":
+    print("You are Harry")
+  else:
+    answer3 = input("Are you a boy? ")
+    if answer3.lower() == "yes":
+      print("You are Ron")
+    else:
+      print("You are Hermione")
