5d5f4d19e35e2c2f715b5fd7b42732613bd9a451
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-09-13 18:45:21 +0100

    Create main.py

---
+++ Day3/main.py
@@ -0,0 +1,9 @@
+# Concatenation
+
+food_type = input("What food do you want to make?\n")
+plant_type = input("What plant could we use to improve it?\n")
+method_cooking = input("What method of cooking will you use?\n")
+burned_food = input("Give a word to describe burned food\n")
+household_item = input("Name a piece of furniture\n")
+
+print("Menu\n", method_cooking, food_type, "with", burned_food, plant_type, "on a", household_item)
