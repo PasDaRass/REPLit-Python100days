52f936dc239f8c8c9e605aa7aefcc26b1aee05c6
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-09-18 18:46:27 +0100

    Update main.py

--- Day6/main.py
+++ Day6/main.py
@@ -1,1 +1,15 @@
+# Using elif
 
+print("Try to brute force a username and password")
+
+username = input("Input your Username: ")
+password = input("Input your Password: ")
+
+if username == "username" and password == "password":
+  print("BORING! but sure, you found one")
+elif username == "Python" and password == "100days":
+  print("Good find! you got one!")
+elif username == "Pas" and password == "byNumbers":
+  print("You got me! Well done finding this one")
+else:
+  print("Login details not found, try try again :]")
