54dc8cfe5f4007a36dfb2893eb7ac8acdd4ed84b
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-09-18 18:51:54 +0100

    Update main.py

--- Day8/main.py
+++ Day8/main.py
@@ -1,1 +1,18 @@
+# if /elif /else dynamic
 
+name = input("What's your name? ")
+day = input("What day of the week is it? ")
+interest = input("What do you love doing? ")
+
+if day.lower() == "monday":
+  print("Hi", name + "! I hope you enjoy", interest, "today.")
+elif day.lower() == "tuesday":
+  print(interest, name + ". What will they type next!?")
+elif day.lower() == "wednesday":
+  print(name, "no cares that you like", interest)
+elif day.lower() == "thursday":
+  print(interest, "who likes", name, "or is it", name, "liked by", interest)
+elif day.lower() == "friday":
+  print("Oh its a good day", name + ". let's try", interest)
+else:
+  print(name, "its the weekend... Programs need breaks too!")
